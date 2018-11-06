37cc465b88f69007ebc90a1ac31c1ddb07ed26b3
Robert Snyder <r.snyder49@gmail.com>
2018-11-06 19:56:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,6 +11,10 @@ class App < Sinatra::Base
     "#{params[:number].to_i * params[:number].to_i}"
   end 
   
+  get '/say/:number/:phrase' do 
+    @num = params[:num]
+    
+  
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
     @str = "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
     @str
