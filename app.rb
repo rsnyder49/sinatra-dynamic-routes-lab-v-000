a9694edbcd2f4b8db7d69db93daf60a999324141
Robert Snyder <r.snyder49@gmail.com>
2018-11-06 19:47:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,8 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do 
-    @reverse_name = param[:name]
-    "<%=#{@reverse_name}.reverse%>"
+    @reverse_name = param[:name].reverse
+    @reverse_name
   end 
   
 end
