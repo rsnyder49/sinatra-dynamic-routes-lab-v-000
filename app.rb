5b5f6d301767df3d712c3c4dedd275a8704d3e9d
Robert Snyder <r.snyder49@gmail.com>
2018-11-06 19:58:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,8 +13,9 @@ class App < Sinatra::Base
   
   get '/say/:number/:phrase' do 
     @num = params[:num]
-    @num.times do params[:phrase]
-  end
+    @num.times 
+      do params[:phrase]
+    end
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
