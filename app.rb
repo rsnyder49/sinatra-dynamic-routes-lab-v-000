c8a9759766e805b6dfba0f78e0ea216bd75cd40d
Robert Snyder <r.snyder49@gmail.com>
2018-11-06 20:03:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,4 +21,11 @@ class App < Sinatra::Base
     @str
   end 
   
+  get '/:operation/:number1/:number2' do 
+    @op = params[:operation]
+    @num1 = params[:number1]
+    @num2 = params[:number2]
+    "#{@num1} #{@op} #{@num2}"
+  end 
+  
 end
