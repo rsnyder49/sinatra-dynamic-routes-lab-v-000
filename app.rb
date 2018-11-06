f0cd0e26eab3ed52d98d26ca68acf5bac420827b
Robert Snyder <r.snyder49@gmail.com>
2018-11-06 20:16:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -31,9 +31,9 @@ class App < Sinatra::Base
     when "subtract"
       "#{@num2} - #{@num1}"
     when "multiply"
-      "@num1 * @num2"
+      "#{@num1} * #{@num2}"
     when "divide"
-      @num1 / @num2
+      "@num1 / @num2"
     end
   end 
   
