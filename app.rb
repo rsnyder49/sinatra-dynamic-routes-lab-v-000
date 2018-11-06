cafe71fb96148c5286ad82bb995e57828e466333
Robert Snyder <r.snyder49@gmail.com>
2018-11-06 19:57:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,6 +15,7 @@ class App < Sinatra::Base
     @num = params[:num]
     @num.times do params[:phrase]
   end
+  end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
     @str = "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
