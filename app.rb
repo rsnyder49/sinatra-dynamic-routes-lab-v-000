0123efe44e5c6a619190a9ac4e3a362b55f20760
Robert Snyder <r.snyder49@gmail.com>
2018-11-06 20:07:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -26,13 +26,13 @@ class App < Sinatra::Base
     @num1 = params[:number1]
     @num2 = params[:number2]
     if @op == "add"
-      "#{@num1} + #{@num2}"
+      @num1 + @num2
     elsif @op == "subtract"
-      "#{@num1} - #{@num2}"
+      @num1 - @num2
     elsif @op == "multiply"
-      "#{@num1} * #{@num2}"
+      @num1 * @num2
     elsif @op == "divide"
-      "#{@num1} / #{@num2}"
+      @num1 / @num2
     end
   end 
   
